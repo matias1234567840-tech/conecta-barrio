5b8a6bade9a1ababaf06893ffc47fd10e73bc8ce
Matias Rosales <matias1234567840@gmail.com>
2026-05-18 20:03:00 -0300

    fix supabase env vars

--- conecta-barrio/src/supabase.js
+++ conecta-barrio/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://pldicwgkjqqriywvwcsl.supabase.co'
-const supabaseKey = 'sb_publishable_Ms5Z185ALro_1gYH747Hpg_5PQS1CZB'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
